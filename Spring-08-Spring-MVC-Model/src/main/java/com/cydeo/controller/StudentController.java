fe764d00c7460c85c501d50712aeb52fb7f369b5
kilanimmed <mohammedkilani17@gmail.com>
2023-09-05 20:25:47 -0400

    SpringBoot MVC - Model

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,17 +1,39 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
     @RequestMapping("/welcome")
-    public String homePage(Model model){
+    public String homePage(Model model){ //Model is an Interface
 
         //my goal here is to be able to call Model's methods, I need to pass Model as parameter
-        model.addAttribute("name","Cydeo");
+        model.addAttribute("name","Cydeo"); // these data belongs to this controller only
         model.addAttribute("course","MVC");
+
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+
+        //create some random studentId(0-1000) and show it in the UI
+        int studentId = new Random().nextInt();
+        model.addAttribute("id",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(10);
+        model.addAttribute("numbers",numbers);
+
+        Student student = new Student(1,"Mike","Smith");
+        model.addAttribute("student",student);
 
 
         return "student/welcome"; //no need to add .html bcs this is a Thymeleaf template
